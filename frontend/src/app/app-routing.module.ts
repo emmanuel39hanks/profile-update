5ff8676bcaf2593ec7424072b315b8b3a9e34c0e
emmanuel39hanks <emmanuel.haankwenda2018@gmail.com>
2022-09-11 13:59:35 +0200

    adding component conditional rendering with the help of Angular JWT to work with the token and added an Auth Guard to add restrictions to protected routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { EditComponent } from './components/edit/edit.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'edit', component: EditComponent}
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'edit', component: EditComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   constructor(
-    private authService: AuthService,
+    public authService: AuthService,
     private alertMessage: AlertMessagesService,
     private router: Router
   ) {}
